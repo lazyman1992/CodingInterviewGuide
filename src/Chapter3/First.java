86e0a4e1f0d7ffc8264c67b9556625d538d118ef
lazyman1992 <shen@DESKTOP-EA1DACT>
2017-01-13 21:10:24 +0800

    add getTwoErrNodes

--- src/Chapter3/First.java
+++ src/Chapter3/First.java
@@ -549,6 +549,30 @@ public class First {
 		
 	}
 	
+	public static Node[] getTwoErrNodes(Node head){
+		Node[] errs =new Node[2];
+		if(head==null)
+			return errs;
+		Stack<Node> stack=new Stack<Node>();
+		Node pre=null;
+		while(!stack.isEmpty()||head!=null){
+			if(head!=null){
+				stack.push(head);
+				head=head.left;
+			}
+			else{
+				head=stack.pop();
+				if(pre!=null&&pre.value>head.value){
+					errs[0]=errs[0]==null?pre:errs[0];
+					errs[1]=head;
+				}
+				pre=head;
+				head=head.right;
+			}
+		}
+		return errs;
+	}
+	
 	public static void main(String[] args){
 //		Node head=new Node(1);
 //		head.left=new Node(2);
